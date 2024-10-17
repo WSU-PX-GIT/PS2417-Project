821ad1accd8733dcf7e2e7ba0f30deeea4ad5ebc
sBui24 <22030234@student.westernsydney.edu.au>
2024-10-18 00:36:49 +1100

    Add files via upload

---
+++ app/Models/CPDReport.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CPDReport extends Model
+{
+    use HasFactory;
+
+    protected $table = 'CPDReport';
+    protected $fillable = [
+        'user_id',
+        'cpd_name',
+        'qualification_id',
+        'cpd_type',
+        'units',
+        'is_cpd_evidence_attached',
+        'cpd_evidence',
+        'CPD_year',
+        'year_completed',
+        'last_updated',
+        'record_status'
+    ];
+
+
+
+}
