59635d429e4089ea3042d99727d936b9feca0b97
sBui24 <22030234@student.westernsydney.edu.au>
2024-10-18 19:07:26 +1100

    Update agenthome.blade.php

--- resources/views/agent/agenthome.blade.php
+++ resources/views/agent/agenthome.blade.php
@@ -1,20 +1,12 @@
 <x-appagent-layout>
-    <x-slot name="header">
+    <div class="main-area">
+
+{{--    <x-slot name="header">--}}
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Agent Dashboard') }}
         </h2>
-    </x-slot>
+        <br>
+        {{ __("Welcome, Agent User!") }}
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Welcome, Agent User!") }}
-                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
-                        {{ __('Agent Dashboard') }}
-                    </x-nav-link>
-                </div>
-            </div>
-        </div>
     </div>
 </x-appagent-layout>
