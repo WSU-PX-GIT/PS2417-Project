6baa191512df15f84c72d3ed6b4e90a7a626cc35
sBui24 <22030234@student.westernsydney.edu.au>
2024-10-18 19:03:58 +1100

    Update adminassign.blade.php

--- resources/views/admin/adminassign.blade.php
+++ resources/views/admin/adminassign.blade.php
@@ -1,19 +1,22 @@
 <x-appadmin-layout>
-
-    <x-slot name="header">
+<div class="main-area">
+{{--    <x-slot name="header">--}}
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Admin Registration') }}
         </h2>
-    </x-slot>
-    <form action="{{ route('user.search') }}" method="GET">
+{{--    </x-slot>--}}
+    <br>
+    <form action="{{ route('user.search') }}" method="GET" class="search_bar">
         <input type="text" name="search" placeholder="Search Users">
         <button type="submit">Search</button>
-
     </form>
-    <h3 class="font-semibold text-xl text-gray-800 leading-tight ml-5 content-center">
+
+    <br>
+    <h3>
         {{ __('Or create Administrator user') }}
     </h3>
     <button><x-nav-link :href="route('admincreate')" :active="request()->routeIs('admincreate')">
         {{ __('Create Admin') }}
     </x-nav-link></button>
+</div>
 </x-appadmin-layout>
