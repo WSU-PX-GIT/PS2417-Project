7284d4b9fb48e6b1e45a803780c581906b0c7b52
Code5ilver <20104955@student.westernsydney.edu.au>
2024-10-18 14:21:45 +1100

    updated view cpd

--- resources/views/admin/cpdmanagement.blade.php
+++ resources/views/admin/cpdmanagement.blade.php
@@ -16,24 +16,25 @@
 
             <th>Qualification</th>
             <th>Region</th>
-            <th>Qualification Classes</th>
             <th>CPD Unit</th>
             <th>Expiry Renewal Date</th>
+            <th>Last Updated</th>
 
         </tr>
-
-            <tr>
                 @if (count($reports) > 0)
-                    <ul>
                         @foreach ($reports as $report)
-                            <li>{{ $report->qualification_name }}</li>
+                    <tr>
+                        <td>{{ $report->qualification_name}}</td>
+                        <td>{{ $report->state_or_territory}}</td>
+                        <td>{{ $report->CPD_unit}}</td>
+                        <td>{{ $report->expiry_renewal_date}}</td>
+                        <td>{{ $report->last_updated }}</td>
+
+                    </tr>
                         @endforeach
-                    </ul>
                 @else
                     <p>No results found.</p>
                 @endif
-
-            </tr>
 
     </table>
 </div>
