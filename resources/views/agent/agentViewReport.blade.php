3529d76dccfeddb662e5d35627b1e7f55fabd03a
sBui24 <22030234@student.westernsydney.edu.au>
2024-10-18 13:13:46 +1100

    Update agentViewReport.blade.php

--- resources/views/agent/agentViewReport.blade.php
+++ resources/views/agent/agentViewReport.blade.php
@@ -15,13 +15,16 @@
         <h2>{{ $report->cpd_name }}</h2>
         <div class="detail-area">
             <section id="document-details">
-            <p><strong>CPD Name:</strong> {{ $report->cpd_name }}</p>
-            <p><strong>Qualification:</strong> {{ $report->qualification_name }}</p>
-            <p><strong>Region:</strong> {{ $report->region }}</p> <!-- assuming a relationship exists -->
-            <p><strong>CPD Type:</strong> {{ $report->cpd_type }}</p>
-            <p><strong>Units:</strong> {{ $report->units }} {{ $report->cpd_unit }}</p>
-            <p><strong>CPD Year:</strong> {{ $report->CPD_year }}</p>
-            <p><strong>Year Completed:</strong> {{ $report->year_completed }}</p>
+                <p><strong>CPD Name:</strong> {{ $report->cpd_name }}</p>
+                <p><strong>Qualification:</strong> {{ $report->qualification_name }}</p>
+                <p><strong>Region:</strong> {{ $report->region }}</p> <!-- assuming a relationship exists -->
+                <p><strong>CPD Type:</strong> {{ $report->cpd_type }}</p>
+                <p><strong>Units:</strong> {{ $report->units }} {{ $report->cpd_unit }}</p>
+                <p><strong>CPD Year:</strong> {{ $report->CPD_year }}</p>
+                <p><strong>Year Completed:</strong> {{ $report->year_completed }}</p>
+                <p><strong>Expiry Date:</strong> {{ $report->expiry_date }}</p>
+                <p><strong>Days until Expired:</strong> {{ (int) \Carbon\Carbon::now()->diffInDays(\Carbon\Carbon::parse($report->expiry_date)) }} Days</p>
+
             </section>
             <br>
 
@@ -36,10 +39,6 @@
             <br>
             <button type="button" onclick="window.location.href='{{ route('editReport', $report->cpd_id) }}';">Edit</button>
 
-{{--            <form method="POST" action="{{ url('agentEditReport') }}">--}}
-{{--                @csrf--}}
-{{--                <button type="submit" name="edit">Edit</button>--}}
-{{--            </form>--}}
         </div>
 
     </div>
