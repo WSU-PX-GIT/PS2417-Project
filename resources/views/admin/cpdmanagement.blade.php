1b26d973c632f9f3331d5d2ba9144ececb0c4863
sBui24 <22030234@student.westernsydney.edu.au>
2024-10-25 16:53:54 +1100

    Update cpdmanagement.blade.php

--- resources/views/admin/cpdmanagement.blade.php
+++ resources/views/admin/cpdmanagement.blade.php
@@ -18,6 +18,7 @@
             <th>Region</th>
             <th>CPD Unit</th>
             <th>Expiry Renewal Date</th>
+            <th>Retention Period</th>
             <th>Last Updated</th>
 
         </tr>
@@ -28,6 +29,7 @@
                         <td>{{ $report->state_or_territory}}</td>
                         <td>{{ $report->CPD_unit}}</td>
                         <td>{{ $report->expiry_renewal_date}}</td>
+                        <td>{{ $report->retention_period}}</td>
                         <td>{{ $report->last_updated }}</td>
 
                     </tr>
