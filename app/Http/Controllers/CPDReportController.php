a4fce82907709c435d64b2f89c651cd8ec89f0c9
Rami Shamon <81132453+RamiShamon@users.noreply.github.com>
2024-10-27 16:14:20 +1100

    Update CPDReportController.php

--- app/Http/Controllers/CPDReportController.php
+++ app/Http/Controllers/CPDReportController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use SebastianBergmann\CodeCoverage\Report\Xml\Report;
-use App\Models\CPDreport;
+use App\Models\CPDReport;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
