648cb16a5de1119482724b0ace533b29ec66e676
sBui24 <22030234@student.westernsydney.edu.au>
2024-10-27 16:21:12 +1100

    Update agentEditReport.blade.php

--- resources/views/agent/agentEditReport.blade.php
+++ resources/views/agent/agentEditReport.blade.php
@@ -31,11 +31,6 @@
             </p>
 
             <p>
-                <label for="Qualification_category">Qualification Category: </label>
-                <input type="text" id="Qualification_category" name="Qualification_category">
-            </p>
-
-            <p>
                 <label for="cpd_type">CPD Type: </label>
                 <input type="radio" id="Compulsory" name="cpd_type" value="Compulsory" {{ $report->cpd_type == 'Compulsory' ? 'checked' : '' }}>
                 <label for="Compulsory">Compulsory</label>
