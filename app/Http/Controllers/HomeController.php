edea6fe85d9f5020724153cd0081ae1358b9bc44
Rami Shamon <81132453+RamiShamon@users.noreply.github.com>
2024-10-27 15:56:12 +1100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use app\Models\User;
-use Illuminate\support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     public function index()
